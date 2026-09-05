f8701b394c9d3105ff673afffb29aa85fc3a6f03
Anser-y <81862592+Anser-y@users.noreply.github.com>
2026-09-06 01:37:02 +0800

    Create board.h

---
+++ trunk/configs/boards/360-T6GS/board.h
@@ -0,0 +1,30 @@
+/* 360-T6GS */
+
+#define BOARD_PID		"360-T6GS"
+#define BOARD_NAME		"360-T6GS"
+#define BOARD_DESC		"Qihoo 360 T6GS Wireless Router"
+#define BOARD_VENDOR_NAME	"Qihoo 360"
+#define BOARD_VENDOR_URL	"http://www.360.cn/"
+#define BOARD_MODEL_URL		"http://life.360.cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_HAS_5G_11AX		1
+#define BOARD_HAS_2G_11AX		1
+#define BOARD_NUM_ANT_5G_TX		2
+#define BOARD_NUM_ANT_5G_RX		2
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+
+/* GPIO 映射配置（提取自 DTS） */
+#define BOARD_GPIO_BTN_RESET		7
+#define BOARD_GPIO_BTN_WPS		6
+#define BOARD_GPIO_LED_ALL_ON		0	/* 低电平点亮 */
+
+#define BOARD_GPIO_LED_STATUS_GREEN	15
+#define BOARD_GPIO_LED_STATUS_RED	13
+#define BOARD_GPIO_LED_STATUS_BLUE	10
